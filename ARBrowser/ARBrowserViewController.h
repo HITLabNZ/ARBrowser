63409be1b4613e0b463ac3a65fd81b5e8c71c3f1
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2011-04-05 16:28:47 +1200

    Initial Commit

---
+++ ARBrowser/ARBrowserViewController.h
@@ -0,0 +1,15 @@
+//
+//  ARBrowserViewController.h
+//  ARBrowser
+//
+//  Created by Samuel Williams on 5/04/11.
+//  Copyright 2011 Orion Transfer Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARBrowserViewController : UIViewController {
+    
+}
+
+@end
---
+++ ARBrowser/ARBrowserViewController.m
@@ -0,0 +1,49 @@
+//
+//  ARBrowserViewController.m
+//  ARBrowser
+//
+//  Created by Samuel Williams on 5/04/11.
+//  Copyright 2011 Orion Transfer Ltd. All rights reserved.
+//
+
+#import "ARBrowserViewController.h"
+
+@implementation ARBrowserViewController
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
