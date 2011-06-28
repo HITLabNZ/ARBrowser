9dd646f179a811dea923e1a7814a3614c390b39d
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2011-06-29 03:33:05 +1200

    Updated existing example code to show how to use metadata property of ARWorldPoint.

--- ARBrowser/ARBrowserViewController.mm
+++ ARBrowser/ARBrowserViewController.mm
@@ -35,31 +35,43 @@
 
 	// Coffee cup model
 	NSString * coffeePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Models/coffee"];
-	ARModel * coffeeCupModel = [[ARModel alloc] initWithName:@"model" inDirectory:coffeePath];
+	id<ARRenderable> coffeeCupModel = [ARModel objectModelWithName:@"model" inDirectory:coffeePath];
 	
-	// Derenzy Pl
+	// 2 Derenzy Pl
 	ARWorldPoint * derenzy = [ARWorldPoint new];
 	location.latitude = -43.516215;
 	location.longitude = 172.554560;
 	[derenzy setCoordinate:location altitude:EARTH_RADIUS];
 	[derenzy setModel:coffeeCupModel];
 	
-	[worldPoints addObject:derenzy];
+	// This name is used for debugging output.
+	[derenzy.metadata setObject:@"2 Derenzy Pl" forKey:@"name"];
 	
+	// This information is printed out below.
+	[derenzy.metadata setObject:@"2 Derenzy Pl" forKey:@"address"];
+	[derenzy.metadata setObject:@"Samuel Williams" forKey:@"developer"];
+	[worldPoints addObject:derenzy];
+		
 	// HitLab NZ
 	ARWorldPoint * hitlab = [ARWorldPoint new];
 	location.latitude = -43.522190;
 	location.longitude = 172.583020;
 	[hitlab setCoordinate:location altitude:EARTH_RADIUS];
 	[hitlab setModel:coffeeCupModel];
+	[hitlab.metadata setObject:@"HITLabNZ" forKey:@"name"];
+	[hitlab.metadata setObject:@"University of Canterbury" forKey:@"address"];
+	[hitlab.metadata setObject:@"Mark Billinghurst" forKey:@"developer"];
 	[worldPoints addObject:hitlab];
-
+	
 	// HitLab NZ
 	ARWorldPoint * cuteCenter = [ARWorldPoint new];
 	location.latitude = 1.29231;
 	location.longitude = 103.775769;
 	[cuteCenter setCoordinate:location altitude:EARTH_RADIUS];
 	[cuteCenter setModel:coffeeCupModel];
+	[cuteCenter.metadata setObject:@"Cute Center" forKey:@"name"];
+	[cuteCenter.metadata setObject:@"Singapore" forKey:@"address"];
+	[cuteCenter.metadata setObject:@"Wang Yuan" forKey:@"developer"];
 	[worldPoints addObject:cuteCenter];
 	
 	_worldPoints = worldPoints;
@@ -103,6 +115,11 @@
 
 - (void) browserView: (ARBrowserView*)view didSelect:(ARWorldPoint*)point {
 	NSLog(@"Browser view did select: %@", point);
+	
+	NSString * developer = [point.metadata objectForKey:@"developer"];
+	NSString * address = [point.metadata objectForKey:@"address"];
+	
+	NSLog(@"Developer %@ at %@", developer, address);
 }
 
 @end
